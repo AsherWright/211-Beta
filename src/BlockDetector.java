3b00d01ae8a20b3326ad3da1dcbc74856f408485
danielebercovici <daniele.bercovici@mail.mcgill.ca>
2015-11-12 17:10:53 -0500

    fixed bugs

--- src/BlockDetector.java
+++ src/BlockDetector.java
@@ -14,15 +14,14 @@ public class BlockDetector extends Thread {
 	//the error that R,G, B can be off for it to still consider it a certain object.
 	private static final double DETECTIONTHRESHOLDERROR = 0.5;
 	private static final double WHEEL_RADIUS = 2.1;
-	private final int DETECTIONTHRESHOLD = 3;
-	private final double centerTolerance = 2; //TODO: test this value
+	private static final double BANDWIDTH = 16.2;
+	private final int DETECTIONRANGE = 4;
 	//color sensor variables
 	private SampleProvider colorSensor;
 	private float[] colorData;
 	//ultrasonic sensor variables
-	private SampleProvider usSensor;
-	double USDistance;
-	private float[] usData;
+	private double USDistance;
+	private UltrasonicPoller frontPoller;
 	//Reading properties
 	private boolean isFlag;
 	private String blockType;
@@ -37,7 +36,7 @@ public class BlockDetector extends Thread {
 	EV3LargeRegulatedMotor verticalArmMotor;
 	EV3LargeRegulatedMotor horizontalArmMotor;
 	
-
+	
 	/**
 	 * Class constructor
 	 * @param colorSensor sample provider for colored sensor
@@ -46,12 +45,12 @@ public class BlockDetector extends Thread {
 	 * @param odo Odometer class Object
 	 * @param leftMotor EV3 Motor on the left 
 	 * @param rightMotor EV3 Motor on the right
-	 * @param usSensor Sample provider for ultrasonic sensor
-	 * @param usData Sample data for Ultrasonic sensor
+	 * @param UltrasonicPoller frontPollerusSensor, sample provider for ultrasonic sensor, usData sample data for Ultrasonic sensor
 	 * @param verticalArmMotor EV3 Motor for up/down movement of arm
 	 * @param horizontalArmMotor EV3 Motor for open/close movement of arm
 	 */
-	public BlockDetector(SampleProvider colorSensor, float[] colorData, Navigation navi, Odometer odo, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,SampleProvider usSensor, float[] usData, EV3LargeRegulatedMotor verticalArmMotor, EV3LargeRegulatedMotor horizontalArmMotor) {
+	public BlockDetector(SampleProvider colorSensor, float[] colorData, Navigation navi, Odometer odo, EV3LargeRegulatedMotor leftMotor, 
+			EV3LargeRegulatedMotor rightMotor,UltrasonicPoller frontPoller, EV3LargeRegulatedMotor verticalArmMotor, EV3LargeRegulatedMotor horizontalArmMotor) {
 		//get incoming values for variables
 		this.colorSensor = colorSensor;
 		this.colorData = colorData;
@@ -59,8 +58,7 @@ public class BlockDetector extends Thread {
 		this.navi= navi;
 		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;
-		this.usData = usData;
-		this.usSensor = usSensor;
+		this.frontPoller = frontPoller;
 		this.verticalArmMotor = verticalArmMotor;
 		this.horizontalArmMotor = horizontalArmMotor;
 		
@@ -84,6 +82,8 @@ public class BlockDetector extends Thread {
 	 * Method that Checks if Block is the right color, set global variable isReadingBlock true if right block and false otherwise.
 	 */
 	public void run(){
+		
+		investigateBlock();
 		while(true){
 			//gets the data from the color sensor.
 			colorSensor.fetchSample(colorData, 0);
@@ -114,7 +114,7 @@ public class BlockDetector extends Thread {
 			}
 			//now sleep so that we don't call this too often.
 			try {
-				Thread.sleep(200);											// sleep for 200 mS
+				Thread.sleep(200);
 			} catch (Exception e) {
 				System.out.println("Error: " + e.getMessage());
 			}
@@ -128,73 +128,94 @@ public class BlockDetector extends Thread {
 		//record initial position
 		odo.getPosition(pos);
 		
-		
-			double lTheta; 
-			double rTheta; 
+		double lTheta; //falling edge angle
+		double rTheta; //rising edge angle
+			
+			
+		USDistance = getFilteredUSData();
+			
+		//turn left till cant see block, record angle
+		while(USDistance <= 15)
+		{
 			USDistance = getFilteredUSData();
+			leftMotor.setSpeed(50);
+			rightMotor.setSpeed(50);
+			rightMotor.forward();
+			leftMotor.backward();	
+		}
+		rightMotor.stop(true);
+		leftMotor.stop(true);
+		Sound.beep();
+		lTheta = pos[2]-odo.getAng();
 			
-			//turn left till cant see block, record angle
-			while(USDistance <= 30)
-			{
-				USDistance = getFilteredUSData();
-				rightMotor.setSpeed(100);
-				rightMotor.forward();
-				
-			}
-			rightMotor.stop();
-			Sound.beep();
-			lTheta = pos[2]-odo.getAng();//record angle (initial-current angle)
-			//navi.travelTo(pos[0], pos[1]);//return to initial position TODO: check if this is needed
-			navi.turnTo(pos[2], false);
-			Sound.beep();
+		navi.turnTo(pos[2], false);
+		Sound.beep();
 			
-			//turn right till cant see block, record angle 
-			while(USDistance <= 30) 
-			{
-				USDistance = getFilteredUSData();
-				leftMotor.setSpeed(100);
-				leftMotor.forward();	
-			}
-			leftMotor.stop();
-			Sound.beep();
-			rTheta = pos[2]-odo.getAng();
-			//navi.travelTo(pos[0], pos[1]);//return to initial position
-			navi.turnTo(pos[2], false); 
-			Sound.beep();
+		USDistance = getFilteredUSData();
+	
+		//turn right till cant see block, record angle 
+		while(USDistance <= 15) 
+		{
+			USDistance = getFilteredUSData();
+			leftMotor.setSpeed(50);
+			rightMotor.setSpeed(50);
+			leftMotor.forward();
+			rightMotor.backward();	
+		}
+		leftMotor.stop(true);
+		rightMotor.stop(true);
+		Sound.beep();
+		rTheta = pos[2]-odo.getAng();
 			
+		navi.turnTo(pos[2], true); 
+			
+		Sound.beep();
+		Sound.beep();
+		rightMotor.setSpeed(50);
+		leftMotor.setSpeed(50);
+		USDistance = getFilteredUSData();
 
-			/*calculate position in relation to the block 
-			 * 1: within center threshold, drive within 3cm detect if its good a) good: pickup b) bad: go back to original position
-			 * 2: too far to right or left, hit block, (back up 3cm?) detect if it is good a) good: pickup b) bad: go back to original position
-			 */
-			double difference = rTheta - lTheta;
+		/*calculate position in relation to the center of block 
+		 * 1: within center threshold, drive within DETECTIONRANGE detect if its good a) good: pickup b) bad: go back to original position 
+		 * 2: too far to right or left, hit block, back up to DETECTIONRANGE, detect if it is good a) good: pickup b) bad: go back to original position
+		 */
+		double difference = Math.abs(rTheta - lTheta);
+		
+		if (difference > 180 ) //take orientation of grid into account
+		{
+			difference = 360-difference;
+		}
 			
-			if(Math.abs(difference) < centerTolerance)//within center tolerance
+		if(difference <= 88 && difference >= 75)//within center range
+		{
+			Sound.buzz();
+			Sound.buzz();
+			
+			USDistance = getFilteredUSData();
+			while(USDistance > DETECTIONRANGE) //get within light sensor range
 			{
+				rightMotor.forward();
+				leftMotor.forward();
 				USDistance = getFilteredUSData();
-				while(USDistance != DETECTIONTHRESHOLD) //get within reading range
-				{
-					rightMotor.forward();
-					leftMotor.forward();
-					USDistance = getFilteredUSData();
-				}
-				rightMotor.stop();
-				leftMotor.stop();
-				
-				isFlagDetected();
-			} 
+			}
+			rightMotor.stop(true);
+			leftMotor.stop(true);
 			
-			else //too far to right or left
-			{
-				//drive into the block to straighten it out
-				//TODO: test 10cm
-				rightMotor.rotateTo(distanceToDegrees(10),false);
-				leftMotor.rotateTo(distanceToDegrees(10),false);
-				
-				//TODO: test light sensor back up 3cm
-				rightMotor.rotateTo(distanceToDegrees(-DETECTIONTHRESHOLD),false);
-				leftMotor.rotateTo(distanceToDegrees(-DETECTIONTHRESHOLD),false);
-				isFlagDetected();
+			isFlagDetected();
+		} 
+			
+		else //too far to right or left
+		{
+			Sound.buzz();
+			
+			//drive into the block to straighten it out
+			rightMotor.rotate(convertDistance(WHEEL_RADIUS,10),true);
+			leftMotor.rotate(convertDistance(WHEEL_RADIUS,10),false);		
+			
+			rightMotor.rotate(convertDistance(WHEEL_RADIUS,-DETECTIONRANGE),true);
+			leftMotor.rotate(convertDistance(WHEEL_RADIUS,-DETECTIONRANGE),false);
+			Sound.beep();
+			isFlagDetected();
 			}
 			
 		}
@@ -206,12 +227,14 @@ public class BlockDetector extends Thread {
 	{
 		if(isFlag) //capture flag
 		{
+			Sound.beep();
 			pickUp();
 		}
 		else //not flag, return to initial position
 		{
+			Sound.beep();
 			navi.travelTo(pos[0], pos[1]);
-			navi.turnTo(pos[3], true);
+			navi.turnTo(pos[2], true);
 		}
 	}
 	
@@ -220,56 +243,83 @@ public class BlockDetector extends Thread {
 	 */
 	public void pickUp() 
 	{
-		//back up to TODO: 20cm
-		while(USDistance <= 20)
+		//back up 
+		while(USDistance <= 13)//arms dont hit at 21cm. perfect dist: 13cm
 		{
 			rightMotor.backward();
 			leftMotor.backward();
 			USDistance = getFilteredUSData();
 		}
+		rightMotor.stop(false);
+		leftMotor.stop(false);
+		
+				
 		//turn around
-		rightMotor.rotate(180);;
-		leftMotor.rotate(-180);
+		rightMotor.rotate(convertAngle(WHEEL_RADIUS,BANDWIDTH,165),true);
+		leftMotor.rotate(convertAngle(WHEEL_RADIUS,BANDWIDTH,-165),false);
+		
+		//open arm
+		horizontalArmMotor.setSpeed(100);
+		horizontalArmMotor.rotate(100, false); 
 		
 		//lower arms
 		verticalArmMotor.setSpeed(100);
-		verticalArmMotor.rotate(-180,false);
+		verticalArmMotor.rotate(-180);
 		try {
-			Thread.sleep(2000);
+		Thread.sleep(2000);
 		} 
 		catch (Exception e) {
 		}
 		
-		//open arm
-		horizontalArmMotor.setSpeed(100);
-		horizontalArmMotor.rotate(100, false); //TODO: does it need time to sleep of can it open up the same time?
+		//back up 
+		rightMotor.rotate(convertDistance(WHEEL_RADIUS,-8),true);//arms dont hit at 15cm, perf dist: -8
+		leftMotor.rotate(convertDistance(WHEEL_RADIUS,-8),false);
 		
-		//back up 20+cm //TODO: test distance
-		rightMotor.rotateTo(distanceToDegrees(-20),false);
-		leftMotor.rotateTo(distanceToDegrees(-20),false);
 		
 		//close arms
-		horizontalArmMotor.rotate(-75, false);
+		horizontalArmMotor.rotate(-130, false);
 		Sound.beep();
-		try {
-			Thread.sleep(2000);
-		} catch (Exception e) {	
-		}
 		
 		//lift up arms
 		verticalArmMotor.rotate(180,false);
 	}
 	
 	/**
-	 * The method converts distance in cm to angle degrees of rotation
-	 * @param distance distance in cm you want to travel (negative for direction)
-	 * @return angle in degrees needed to turn to travel given distance
+	 * This method converts a distance to an angle in degrees
+	 * @param radius the wheel radius
+	 * @param distance desired distance to convert
+	 * @return angle in degrees
 	 */
-	public int distanceToDegrees(double distance)
-	{
-		int theta = (int) (distance/WHEEL_RADIUS); //theta = s/r
-		theta = (int) (theta *180/Math.PI); //convert radians to degrees
-		return theta;
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+
+	/**
+	 * This method converts an angle to distance in cm
+	 * @param radius the wheel radius
+	 * @param width the bandwidth of the two wheels 
+	 * @param angle desired angle to convert
+	 * @return result of method convertDistanc() distance in cm
+	 */
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+	/**
+	 * This method filters the Ultrasonic Data. clips data if too far
+	 * @return result
+	 */
+	private float getFilteredUSData() {
+		float distance = frontPoller.getUsData();
+		float result = 0;
+		if (distance > 200){
+			// true 255, therefore set distance to 255
+			result = 200; //clips it at 50
+		} else {
+			// distance went below 255, therefore reset everything.
+			result = distance;
+		}
+		//lastDistance = distance;
+		return result;
 	}
 
 	//accessors
@@ -290,19 +340,6 @@ public class BlockDetector extends Thread {
 		}
 	}
 
-	private float getFilteredUSData() {
-		usSensor.fetchSample(usData, 0);
-		float distance = (int)(usData[0]*100.0);
-		float result = 0;
-		if (distance > 200){
-			// true 255, therefore set distance to 255
-			result = 200; //clips it at 50
-		} else {
-			// distance went below 255, therefore reset everything.
-			result = distance;
-		}
-		//lastDistance = distance;
-		return result;
-	}
+	
 
 }
